0f480ffd5d4dfcf54a867d9cf61c961e160aa522
Landon Redmond <landon.redmond0@gmail.com>
2024-04-19 11:19:25 -0500

    Improve plugin.

--- camera/plugins/faces/overlay-zoom/main.py
+++ camera/plugins/faces/overlay-zoom/main.py
@@ -19,24 +19,21 @@ class Plugin:
     def __newFrame(self, frame):
         faces = detector.detectFaces(frame)
 
-        if faces:
-            (start_x, start_y, end_x, end_y) = faces[0]
-
-            face_roi = frame[start_y:end_y, start_x:end_x]
+        for startX, startY, endX, endY in faces:
+            face_roi = frame[startY:endY, startX:endX]
 
             zoomed_face = resize(face_roi, None, fx=ZOOM_FACTOR, fy=ZOOM_FACTOR)
 
-            new_start_x = max(
-                start_x - int((ZOOM_FACTOR - 1) * (end_x - start_x) / 2), 0
+            startX = max(
+                startX - int((ZOOM_FACTOR - 1) * (endX - startX) / 2), 0
             )
-            new_start_y = max(
-                start_y - int((ZOOM_FACTOR - 1) * (end_y - start_y) / 2), 0
+            startY = max(
+                startY - int((ZOOM_FACTOR - 1) * (endY - startY) / 2), 0
             )
-            new_end_x = min(new_start_x + zoomed_face.shape[1], frame.shape[1])
-            new_end_y = min(new_start_y + zoomed_face.shape[0], frame.shape[0])
+            
+            endX = min(startX + zoomed_face.shape[1], frame.shape[1])
+            endY = min(startY + zoomed_face.shape[0], frame.shape[0])
 
-            frame[new_start_y:new_end_y, new_start_x:new_end_x] = zoomed_face[
-                : new_end_y - new_start_y, : new_end_x - new_start_x
-            ]
+            frame[startY:endY, startX:endX] = zoomed_face[:endY - startY, :endX - startX]
 
         return frame
