10606f3842cb3f97654c223c9fd7d6780331842c
Landon Redmond <landon.redmond0@gmail.com>
2024-04-19 10:59:34 -0500

    Fix callback name.

--- camera/plugins/filters/grayscale/main.py
+++ camera/plugins/filters/grayscale/main.py
@@ -10,10 +10,10 @@ class Plugin:
         pass
 
     def load(self):
-        webcam.addNewFrameCallback("greyscale", self.__newFrame, 4)
+        webcam.addNewFrameCallback("grayscale", self.__newFrame, 4)
 
     def unload(self):
-        webcam.removeNewFrameCallback("greyscale")
+        webcam.removeNewFrameCallback("grayscale")
 
     def __newFrame(self, frame):
         return cvtColor(frame, COLOR_BGR2GRAY)
