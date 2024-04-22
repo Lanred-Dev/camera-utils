69cc77e86712a8c86ab620823d2b1cde40606eca
Landon Redmond <landon.redmond0@gmail.com>
2024-04-22 11:14:45 -0500

    Improve Container component.

--- app/components/Container.py
+++ app/components/Container.py
@@ -1,16 +1,20 @@
 from sys import modules
 
-from PySide6.QtWidgets import QFrame, QVBoxLayout
+from PySide6.QtWidgets import QFrame, QVBoxLayout, QHBoxLayout
 
 class Container(QFrame):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None, layout="v"):
+        super().__init__(parent)
         
         self.setStyleSheet(
             "background-color: #101112; border-radius: 10px; color: #ffffff; padding: 22px;"
         )
-
-        self.layout = QVBoxLayout(self)
+        
+        if layout == "v":
+            self.layout = QVBoxLayout(self)
+        else:
+            self.layout = QHBoxLayout(self)
+            
         self.layout.setContentsMargins(0, 0, 0, 0)
         self.layout.setSpacing(3)
         self.setLayout(self.layout)
